e6e16e2c09334911b622119fb36619d4257e6157
Дмитрий Наздрин <dmitrijnazdrin@Noutbuk-Dmitrij.local>
2025-01-12 08:33:34 +0200

    Update config.js for the test environment

--- admin-panel/src/config.js
+++ admin-panel/src/config.js
@@ -1,6 +1,6 @@
 // Импорт значений из переменных окружения
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000/developer_panel";
-const DATABASE_URL = process.env.DATABASE_URL || "postgresql+asyncpg://postgres:local_password@localhost/inventory_db";
+const DATABASE_URL = process.env.DATABASE_URL || "postgresql+asyncpg://postgres:your_password@localhost/inventory_db";
 
 // Экспорт переменных для использования в других модулях
 export { API_BASE_URL, DATABASE_URL };
